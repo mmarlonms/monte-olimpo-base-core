32b808e834c4dfdefd942e6cce15e53695273d99
Marlon Miranda <marlon_kkkk@hotmail.com>
2019-09-14 12:33:37 -0300

    Ajuste no método de delete

--- src/MonteOlimpo.Base.Core.Data/Repository/BaseRepository.cs
+++ src/MonteOlimpo.Base.Core.Data/Repository/BaseRepository.cs
@@ -28,7 +28,7 @@ namespace MonteOlimpo.Base.Core.Data.Repository
 
         public void Delete(T entity)
         {
-            T existing = this.unitOfWork.Context.Set<T>().Find(T.Id);
+            T existing = this.unitOfWork.Context.Set<T>().Find(entity.Id);
             if (existing != null) this.unitOfWork.Context.Set<T>().Remove(existing);
             this.unitOfWork.Commit();
         }
