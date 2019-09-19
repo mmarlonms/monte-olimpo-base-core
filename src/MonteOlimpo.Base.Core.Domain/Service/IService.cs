a5ccbcde0ca24b6bc00845157363d8802f7563d7
Marlon Miranda <marlon_kkkk@hotmail.com>
2019-09-19 10:18:25 -0300

    Inclusão da Camada de Base Service.

---
+++ src/MonteOlimpo.Base.Core.Domain/Service/IService.cs
@@ -0,0 +1,15 @@
+﻿using MonteOlimpo.Base.Core.Domain.Model;
+using System.Collections.Generic;
+
+namespace MonteOlimpo.Base.Core.Domain.Service
+{
+    public interface IService<T>
+        where T: ModelBase
+    {
+        IList<T> GetAll();
+        T GetByID(int id);
+        T Create(T modelBase);
+        T Update(T modelBase);
+        void Delete(int id);
+    }
+}
---
+++ src/MonteOlimpo.Base.Core.Service/BaseService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MonteOlimpo.Base.Core.Domain.Model;
+using MonteOlimpo.Base.Core.Domain.Repository;
+using MonteOlimpo.Base.Core.Domain.Service;
+
+namespace MonteOlimpo.Base.Core.Service
+{
+    public class BaseService<T> : IService<T>
+        where T : ModelBase
+    {
+        private readonly IRepository<T> repository;
+
+        public BaseService(IRepository<T> repository)
+        {
+            this.repository = repository;
+        }
+
+        public T Create(T modelBase)
+        {
+            this.repository.Add(modelBase);
+            return modelBase;
+        }
+
+        public void Delete(int id)
+        {
+            var item = this.repository.GetById(id);
+            if (item != null)
+                this.repository.Delete(item);
+        }
+
+        public IList<T> GetAll()
+        {
+            return this.repository.List().ToList();
+        }
+
+        public T GetByID(int id)
+        {
+            return this.repository.GetById(id);
+        }
+
+        public T Update(T modelBase)
+        {
+            this.repository.Update(modelBase);
+            return modelBase;
+        }
+    }
+}
